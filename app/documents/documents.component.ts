954600c8be8d50c77f3fbcac6be9c8b92aec42a5
chaneyventura <chaneyventura@gmail.com>
2017-03-02 13:35:55 -0700

    Initial commit

---
+++ app/documents/documents.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Document } from './document';
+
+@Component({
+  moduleId: module.id,
+  selector: 'documents',
+  templateUrl: 'documents.component.html'
+})
+export class DocumentsComponent {
+  pageTitle: string = "Document Dashboard"
+  
+  documents: Document[] = [
+  {
+    title: "My First Doc",
+    description: "description",
+    file_url: 'http://google.com',
+    updated_at: '03/02/2017',
+    image_url: 'http://google.com',
+  },
+  {
+    title: "My Second Doc",
+    description: "description",
+    file_url: 'http://google.com',
+    updated_at: '03/02/2017',
+    image_url: 'http://google.com',
+  },
+  {
+    title: "My Last Doc",
+    description: "description",
+    file_url: 'http://google.com',
+    updated_at: '03/02/2017',
+    image_url: 'http://google.com',
+  }
+ ]
+}
---
+++ app/proposal/proposal-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Proposal } from './proposal';
+
+@Component({
+  moduleId: module.id,
+  selector: 'proposal-list',
+  templateUrl: 'propsal-list.component.html'
+})
+export class ProposalListComponent {
+  proposalOne: Proposal = new Proposal(15, 'ABC Company', 'http://chaneyventura.com', 'Ruby on Rails', 150, 120, 15, 'chaney@gmail.com')
+  proposalTwo: Proposal = new Proposal(99, 'XYZ Company', 'http://chaneyventura.com', 'Ruby on Rails', 150, 120, 15, 'chaney@gmail.com')
+  proposalThree: Proposal = new Proposal(300, 'Something Company', 'http://chaneyventura.com', 'Ruby on Rails', 150, 120, 15, 'chaneyventura@gmail.com')
+
+  proposals: Proposal[] = [
+  this.proposalOne, 
+  this.proposalTwo, 
+  this.proposalThree
+  ]
+}
