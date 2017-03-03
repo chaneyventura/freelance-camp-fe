9e616a834ba792fbc7f2e8d6f7d25d0bdda04be6
chaneyventura <chaneyventura@gmail.com>
2017-03-03 11:40:33 -0700

    Add Bootstrap cards to Documents, Add CSS styles

--- app/documents/documents.component.js
+++ app/documents/documents.component.js
@@ -16,21 +16,21 @@ var DocumentsComponent = (function () {
                 description: "description",
                 file_url: 'http://google.com',
                 updated_at: '03/02/2017',
-                image_url: 'http://google.com',
+                image_url: 'https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/581802_611403818872286_1518120101_n.jpg?oh=4d826f87de92653d828ec9ddd7c20201&oe=596AA638',
             },
             {
                 title: "My Second Doc",
                 description: "description",
                 file_url: 'http://google.com',
                 updated_at: '03/02/2017',
-                image_url: 'http://google.com',
+                image_url: 'https://scontent-lax3-2.xx.fbcdn.net/v/t31.0-8/16487094_10155056568179588_7206370815830926266_o.jpg?oh=243ab5fc5d2a2b77bf98382886ee9274&oe=5932CE1F',
             },
             {
                 title: "My Last Doc",
                 description: "description",
                 file_url: 'http://google.com',
                 updated_at: '03/02/2017',
-                image_url: 'http://google.com',
+                image_url: 'https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/15590528_361653157541816_2976226373541435899_n.png?oh=8edc05497ca69f8ef0a0529dcb14102f&oe=59686580',
             }
         ];
     }
@@ -40,7 +40,8 @@ DocumentsComponent = __decorate([
     core_1.Component({
         moduleId: module.id,
         selector: 'documents',
-        templateUrl: 'documents.component.html'
+        templateUrl: 'documents.component.html',
+        styleUrls: ['documents.component.css']
     })
 ], DocumentsComponent);
 exports.DocumentsComponent = DocumentsComponent;
